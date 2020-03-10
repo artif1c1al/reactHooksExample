99d30e38f67518b521012e0d16c0fcd429982f94
artif1c1al <azhur200@rambler.ru>
2020-03-10 21:26:31 +0300

    useReduce

--- src/App.js
+++ src/App.js
@@ -1,59 +1,26 @@
-import React, { Component, useState, useEffect } from 'react'
+import React, { Component, useState, useEffect, useReducer } from 'react'
 import TodoList from './TodoList'
 import {Context} from './context'
+import reducer from './reducer'
 
 export default function App() {
 
-  const [todos, setTodos] = useState([
-    {id: 1, title: 'First todo', completed: false},
-    {id: 2, title: 'Second todo', completed: true},
-  ])
-
   const [todoTitle, setTodoTitle] = useState('')
-  
 
-  useEffect(() => {
-    const raw = localStorage.getItem('todos') || []
-    setTodos(JSON.parse(raw))
-  }, []);
+  const [state, dispatch] = useReducer(reducer, 
+    JSON.parse(localStorage.getItem('todos'))
+  )  
   
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-  }, [todos]);
+    localStorage.setItem('todos', JSON.stringify(state))
+  }, [state]);
 
 
-  const addTodo = (e) => {
-    if(e.key == "Enter"){
-      setTodos([
-        ...todos,
-        {
-          id: Date.now(),
-          title: todoTitle,
-          completed: false,
-        }
-      ])
-      setTodoTitle('');
-    }
-  }
-
-  const toggleTodo = (id) => {
-    setTodos(todos.map( todo => {
-      if(todo.id === id){
-        todo.completed = !todo.completed;
-      }
-      return todo
-    }));
-  }
-
-  const removeTodo = id => {
-    setTodos(todos.filter(todo => (
-      todo.id !== id
-    )));
-  }
 
   return (
-    <Context.Provider 
-    value = {{ toggleTodo, removeTodo }}>
+    <Context.Provider
+    value = {{ dispatch }}
+    >
       <div className="container">
         <h1>Todo app</h1>
 
@@ -61,12 +28,20 @@ export default function App() {
             <input type="text"
             value = {todoTitle} 
             onChange = { e => setTodoTitle(e.target.value) }
-            onKeyPress = {e => addTodo(e)}
+            onKeyPress = {e => {
+              if(e.key === "Enter"){
+                dispatch({
+                  type: 'add',
+                  payload: todoTitle
+                })
+                setTodoTitle('')
+              }
+            }} 
             />
             <label>Todo name</label>
           </div>
 
-          <TodoList todos={todos} />
+          <TodoList todos={state} />
       </div>
     </Context.Provider>
   );
